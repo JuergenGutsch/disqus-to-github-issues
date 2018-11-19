5f52eda897e848e7ec91006956f0374069dd50ea
Juergen Gutsch <juergen@gutsch-online.de>
2018-11-19 07:38:29 +0100

    initial commit

---
+++ DisqusToGithubIssues/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DisqusToGithubIssues
+{
+    public static class StringExtensions
+    {
+        public static int ToInteger(this string value)
+        {
+            if (Int32.TryParse(value, out int result))
+            {
+                return result;
+            }
+            return 0;
+        }
+
+        public static DateTime ToDateTime(this string value)
+        {
+            if (DateTime.TryParse(value, out DateTime result))
+            {
+                return result;
+            }
+            return DateTime.Now;
+        }
+    }
+}
---
+++ DisqusToGithubIssues/XmlNodeExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Xml;
+
+namespace DisqusToGithubIssues
+{
+    public static class XmlNodeExtensions
+    {
+        public static T NodeValue<T>(this XmlNode node)
+        {
+            return node.NodeValue<T>(default(T));
+        }
+        public static T NodeValue<T>(this XmlNode node, T def)
+        {
+            if (node == null)
+            {
+                return def;
+            }
+            try
+            {
+                var value = node.NodeValue();
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch (Exception)
+            {
+                return def;
+            }
+        }
+        public static string NodeValue(this XmlNode node)
+        {
+            var value = node.FirstChild?.Value ?? String.Empty;
+            return value.Trim();
+        }
+        public static T AttributeValue<T>(this XmlNode node, int index)
+        {
+            return node.AttributeValue(index, default(T));
+        }
+        public static T AttributeValue<T>(this XmlNode node, int index, T def)
+        {
+            if (node == null)
+            {
+                return def;
+            }
+            if (node.Attributes.Count == 0)
+            {
+                return def;
+            }
+            try
+            {
+                var value = node.AttributeValue(index);
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch (Exception ex)
+            {
+                return def;
+            }
+        }
+        public static string AttributeValue(this XmlNode node, int index)
+        {
+            var value = node.Attributes[index]?.Value;
+            return value.Trim();
+        }
+    }
+}
